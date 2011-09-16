21c6438e339957ab512e06230946959b867d8fd8
Alessandro Berardi <berardialessandro@gmail.com>
2011-09-16 11:46:31 +0930

    Updated specs to reflect new logic

--- spec/command_spec.rb
+++ spec/command_spec.rb
@@ -2,7 +2,7 @@ require 'command/command'
 require 'command/proc_command'
 
 describe Command::Command, "upon instantiation" do
-  it "should result non executed" do
+  it "should result not executed" do
     Command::Command.new.should_not be_executed
   end
 end
@@ -13,14 +13,22 @@ describe Command::Command , "when executing" do
     3.times do
       command = Command::Command.new
       command.stub(:execute_command).and_return(true)
+      command.stub(:unexecute_command).and_return(true)
       @working_commands << command
     end
     @failing_commands = []
     3.times do
       command = Command::Command.new
       command.stub(:execute_command).and_return(false)
+      command.stub(:unexecute_command).and_return(true)
       @failing_commands << command
     end
+  end
+  
+  it "should not execute if already executed" do
+    @working_commands[0].execute
+    @working_commands[0].should_not_receive(:execute_command)
+    @working_commands[0].execute.should be_false
   end
   
   it "should result executed if executed successfully" do
@@ -53,6 +61,20 @@ describe Command::Command , "when executing" do
     @working_commands[0].add_children(@working_commands[1], @failing_commands[0], @working_commands[2])
     @working_commands[0].should_not_receive(:execute_command)
     @working_commands[0].execute.should be_false
+    @working_commands[0].should_not be_executed
+  end
+  
+  it "should return the list of executed commands" do
+    @working_commands[0].add_children(@working_commands[1], @failing_commands[0], @working_commands[2])
+    @working_commands[0].execute
+    @working_commands[0].executed.should eql [@working_commands[1], @working_commands[2]]
+  end
+  
+  it "should return the list of not executed commands" do
+    @working_commands[0].add_children(@working_commands[1], @failing_commands[0], @working_commands[2])
+    @working_commands[0].execute
+    @working_commands[0].unexecute
+    @working_commands[0].not_executed.should eql [@working_commands[0], @working_commands[1], @failing_commands[0], @working_commands[2]]
   end
 end
 
@@ -74,9 +96,9 @@ describe Command::Command , "when unexecuting" do
     end
   end
   
-  it "should not unexecute if not previously executed" do
+  it "should unexecute without calling the unexecute_command method if not executed" do
     @working_commands[0].should_not_receive(:unexecute_command)
-    @working_commands[0].unexecute.should be_false
+    @working_commands[0].unexecute.should be_true
   end
   
   it "should unexecute if previously executed" do
@@ -118,8 +140,22 @@ describe Command::Command , "when unexecuting" do
   it "should not unexecute successfully if at least one of the child commands is not unexecuted successfully" do
     @working_commands[0].add_children(@working_commands[1], @failing_commands[0], @working_commands[2])
     @working_commands[0].execute
-    @working_commands[0].should_not_receive(:unexecute_command)
+    @working_commands[0].should_receive(:unexecute_command).once.with(no_args)
+    @working_commands[0].should be_executed
     @working_commands[0].unexecute.should be_false
+  end
+  
+  it "should return the list of executed commands" do
+    @working_commands[0].add_children(@working_commands[1], @failing_commands[0], @working_commands[2])
+    @working_commands[0].execute
+    @working_commands[0].executed.should eql [@working_commands[0], @working_commands[1], @failing_commands[0], @working_commands[2]]
+  end
+  
+  it "should return the list of not executed commands" do
+    @working_commands[0].add_children(@working_commands[1], @failing_commands[0], @working_commands[2])
+    @working_commands[0].execute
+    @working_commands[0].unexecute
+    @working_commands[0].not_executed.should eql [@working_commands[1], @working_commands[2]]
   end
 end
 
@@ -151,11 +187,11 @@ describe Command::ProcCommand, "when unexecuting" do
   before do
     @working_command = Command::ProcCommand.new(lambda {true}, lambda {true})
     @failing_command = Command::ProcCommand.new(lambda {true}, lambda {false})
-    @non_unexecutable_command = Command::ProcCommand.new(lambda {true})
+    @not_unexecutable_command = Command::ProcCommand.new(lambda {true})
     
     @working_command.execute
     @failing_command.execute
-    @non_unexecutable_command.execute
+    @not_unexecutable_command.execute
   end
   
   it "should result not executed if the proc is unexecuted successfully" do
@@ -169,7 +205,7 @@ describe Command::ProcCommand, "when unexecuting" do
   end
   
   it "should not unexecute if the unexecute proc is not present" do
-    @non_unexecutable_command.unexecute.should be_false
-    @non_unexecutable_command.should be_executed
+    @not_unexecutable_command.unexecute.should be_false
+    @not_unexecutable_command.should be_executed
   end
 end
